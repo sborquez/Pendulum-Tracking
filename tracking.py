ec3bd0dfad037e7e358b7a1d9f0a0ad3ca9e89ae
sborquez <sebastian.borquez@gmail.com>
2018-12-09 19:22:30 -0300

    add labels

--- tracking.py
+++ tracking.py
@@ -1,3 +1,4 @@
+from time import sleep
 import sys
 import os
 from os import path
@@ -50,8 +51,9 @@ points = []
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 
 p0 = np.array([[[100,263]],[[362,266]]], dtype = np.float32)
-
 points.append(p0)
+x0 = p0[0,0,0]
+y0 = p0[0,0,1]
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
@@ -77,19 +79,24 @@ while(True):
 
     # calculate optical flow
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    x1 = p1[0,0,0]
+    y1 = p1[0,0,1]
 
     # Update x range and y range
-    if (p1[0,0,0] > max_x): max_x = p1[0,0,0]
-    if (p1[0,0,1] > max_y): max_y = p1[0,0,1]
-    if (p1[0,0,0] < min_x): min_x = p1[0,0,0]
-    if (p1[0,0,1] < min_y): min_y = p1[0,0,1]
+    if (x1 > max_x): max_x = x1
+    if (y1 > max_y): max_y = y1
+    if (x1 < min_x): min_x = x1
+    if (y1 < min_y): min_y = y1
 
     #print(min_x, max_x, min_y, max_y)
     #input("escriba algo: (:v)")
     # Select good points
     #print(p1)
     points.append(p1) #iframe == len(points)
+
+    # UPDATE VARIABLES
     if (iframe%interval_frames == 0 and updates):
+        
         l, ox, oy, fx, fy = calculate_center(points, (min_x, max_x), (min_y, max_y), fps, False)
         updates -= 1
 
@@ -107,9 +114,20 @@ while(True):
     cv2.circle(img,(min_x,min_y),6, (0,0,255),-1)
     cv2.circle(img,(max_x,min_y),6, (0,0,255),-1)
     cv2.circle(img,(int((min_x + max_x)/2),max_y),6, (0,0,255),-1)
+    
+    # Draw pendulum center
     if ox is not None:
         cv2.circle(img, (ox, oy), 5, (255,255,255),-1)
-    
+
+    # Draw values
+    #draw(img, x1, y1, ox, oy,  y1-y0)
+    cv2.arrowedLine(img, (x1, y1),(int(x1 + 5*(x1-x0)), int(y1 + 5*(y1-y0))), (0,125,255), 4)
+
+    # Add labels
+    msg = f"x: {int(x1)}    y: {int(y1)}    vx: {int(x1-x0)}    vy: {int(y1-y0)}    theta: -"
+    cv2.putText(img, msg, (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255))
+    cv2.putText(img, f"frame: {iframe}" , (10, img.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255))
+
     cv2.imshow('frame',img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -118,6 +136,10 @@ while(True):
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
+    x0, y0 = x1, y1
+    if source == "video":
+        sleep(1/fps)
+
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
